c7a51bca79e1ca17d6398230b4af8cc8c083bbcc
161710157 <161710157@smkassalaambandung.sch.id>
2017-10-24 21:38:47 +0700

    latihan keempat saya

---
+++ tugas1.php
@@ -0,0 +1,12 @@
+<?php
+
+class robot{
+public $suara ="ngik ngik ngik";
+public $berat =30;
+
+}
+$robot = new robot;
+echo "berat robot :".$robot->berat;
+echo "bunyi robot :".$robot->suara;
+
+?>
